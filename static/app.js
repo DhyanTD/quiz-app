6f1807f2f32bd10807403eb4fb2057ba70fc8497
DhyanTD <69795325+DhyanTD@users.noreply.github.com>
2023-01-13 01:30:05 +0530

    h

--- static/app.js
+++ static/app.js
@@ -519,7 +519,7 @@ const ava = ({ icon = 'success', toast = false, progressBar = true, text = null,
 window.addEventListener('blur', function() {
     ava({
         icon: 'danger',
-        text: 'Warning! Do not Switch tabs/window. You are being monitored!',
+        text: 'Warning! <br> Do not Switch tabs/window. You are being monitored!',
         btnText: 'Okay',
         progressBar: true,
         toast: false,
